c853e2c357defe2f2dbaca84ed83f159bfc20050
lsjpjs1 <lsjpjs1@naver.com>
2022-09-08 03:56:34 +0900

    feat: 네비게이션, 바텀 탭 기본 설정 추가

--- app/src/main/java/com/hotdealnoti/MainActivity.kt
+++ app/src/main/java/com/hotdealnoti/MainActivity.kt
@@ -2,17 +2,30 @@ package com.hotdealnoti
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.hotdealnoti.ui.main.MainFragment
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.hotdealnoti.databinding.MainActivityBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding: MainActivityBinding by lazy {
+        MainActivityBinding.inflate(layoutInflater)
+    }
 
+
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, MainFragment.newInstance())
-                .commitNow()
-        }
+        setContentView(binding.root)
+        setupJetpackNavigation()
+    }
+
+
+    private fun setupJetpackNavigation() {
+        val host =
+            supportFragmentManager.findFragmentById(R.id.main_nav_host_fragment) as NavHostFragment
+
+        navController = host.navController
+        binding.bottomNavigationView.setupWithNavController(navController)
     }
 }
--- app/src/main/java/com/hotdealnoti/ui/main/MainViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.hotdealnoti.ui.main
-
-import androidx.lifecycle.ViewModel
-
-class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
